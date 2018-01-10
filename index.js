50d67383e19c921df2ba26b1b660541e16f2ca4a
carlo <lmedina@wesleyan.edu>
2018-01-10 07:54:23 -0500

    added some functionality re dictionary

--- index.js
+++ index.js
@@ -5,27 +5,85 @@ const bodyParser = require("body-parser");
 
 
 const danklodictionary = {
-  "kuya" : "orb",
-  "bro" : "orb",
-  "extreme" : "petmalu",
-  "exceptional" : "petmalu",
-  "cool" : "petmalu",
-  "excellent" : "petmalu",
-  "mom" : "mumshie",
-  "mama" : "mumshie",
-  "nanay" : "mumshie",
-  "salamat" : "matsala",
-  "thank" : "matsala", // think of a way on how to make this handle thank you
-  "pare" : "repa",
-  "busog" : "sogbu",
-  "sarap" : "rapsa",
-  "bes" : "beshie",
-  "hindi" : "dehins",
-  "hinde" : "dehins",
-  "barkada" : "dabarkads",
-  "friends" : "dabarkads",
+  "kuya" : {
+    "word" : ["orb"],
+    "rating" : 999
+    },
+  "bro" : {
+    "word" : ["orb"],
+    "rating" : 999
+    },
+  "extreme" : {
+    "word" : ["petmalu"],
+    "rating" : 999
+    },
+  "exceptional" : {
+    "word" : ["petmalu"],
+    "rating" : 999
+    },
+  "cool" : {
+    "word" : ["petmalu"],
+    "rating" : 999
+    },
+  "excellent" : {
+    "word" : ["petmalu"],
+    "rating" : 999
+    },
+  "mom" : {
+    "word" : ["mumshie"],
+    "rating" : 999
+    },
+  "mama" : {
+    "word" : ["mumshie"],
+    "rating" : 999
+    },
+  "nanay" : {
+    "word" : ["mumshie"],
+    "rating" : 999
+    },
+  "salamat" : {
+    "word" : ["matsala"],
+    "rating" : 999
+    },
+  "thank" : {
+    "word" : ["matsala"],
+    "rating" : 999
+    },// think of a way on how to make this handle thank you
+  "pare" : {
+    "word" : ["repa"],
+    "rating" : 999
+    },
+  "busog" : {
+    "word" : ["sogbu"],
+    "rating" : 999
+    },
+  "sarap" : {
+    "word" : ["rapsa"],
+    "rating" : 999
+    },
+  "bes" : {
+    "word" : ["beshie"],
+    "rating" : 999
+    },
+  "hindi" : {
+    "word" : ["dehins"],
+    "rating" : 999
+    },
+  "hinde" : {
+    "word" : ["dehins"],
+    "rating" : 999
+    },
+  "barkada" : {
+    "word" : ["dabarkads"],
+    "rating" : 999
+    },
+  "friends" : {
+    "word" : ["dabarkads"],
+    "rating" : 999
+    }
 }
 
+var danklodictionarytemp
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -38,29 +96,59 @@ app.get("/", function(req, res){
   res.sendFile(__dirname + "/main.html")
 });
 
-  // endpoint to send text to translate
+  // ENDPOINT to send text to translate
 app.post("/translate", function(req, res){
-  const text2translateArray = req.body.text.split(" ")
-  const translatedArray = text2translateArray.map(function(curr, ind, arr) {
-    if (danklodictionary[curr]) {
-      return danklodictionary[curr.toLowerCase()]
+  res.send(lodify(req.body.text))
+});
+
+
+
+  // ENDPOINT to give translation
+app.post("/add2dictionary", function(req, res){
+  const key = req.body.nonlodi
+  const value = req.body.lodi
+  const rating = 0
+
+  // check if not in the dictionary
+  // push the keyvalue pair to database
+  if (!danklodictionary[key]) {
+    var entry = {
+      "word" : [value],
+      "rating" : rating
     }
-    return curr
-  })
-  res.send(translatedArray.join(" "))
+    // add to temp dict
+    danklodictionarytemp[key] = entry
+  }
+
+  // else check if the lodi word is also in the list of lodi words
+
+
+
+
+
+
+
+  res.send("[INFO]: Successfully added to dictionary")
 });
 
-  // endpoint to give translation
-app.post("/dictionary", function(req, res){
-  
+app.get("/dictionary", function(req, res) {
+  res.json(danklodictionary)
+})
 
-});
 
-  // endpoint to get random words to rate
+  // ENDPOINT to get random words to rate
 app.get("/rate", function(req, res){
-  
+  // call dictionary for words with rate less than threshold
 
+  // send the keyvalue pairs back to client
 });
+
+
+
+  // ENDPOINT to update the rating of the words
+app.post("/rate", function(req, res) {
+
+})
 
 
 // HELPER FUNCTIONS
@@ -70,6 +158,68 @@ function shuffleArray(array) {
         [array[i], array[j]] = [array[j], array[i]];
     }
 }
+
+function isVowel(letter) {
+  const vowel = ['a', 'e', 'i', 'o', 'u']
+  return (vowel.includes(letter) ? 'p' : 'k')
+}
+
+function vowelize(word) {
+  const letterlist = word.split("")
+  return letterlist.map(isVowel)
+}
+
+function lodifyWord(word) {
+  if (word.split("").length < 3) {
+    return word
+  }
+  // check if in dictionary
+    if (danklodictionary[word.toLowerCase()]) {
+      return danklodictionary[word.toLowerCase()].word[0]
+    }
+
+  // else create it on the fly 
+    else {
+      var rand = Math.random()
+      // if random number is greater than some threshold, do it
+      if (rand > 0.9) {
+        const pantig = ['p', 'kp', 'pk', 'kpk', 'pkk', 'kkp', 'kkpk', 'kpkk', 'kkpkkk']
+        const pantiglist = vowelize(word)
+        const letterlist = word.split("")
+        var curr = pantiglist[pantiglist.length-1];
+        var ready = false
+        for (let i=pantiglist.length-2; i>-1; i--) {
+          var newpantig = pantiglist[i]+curr;
+          if (pantig.includes(newpantig)) {
+            ready = true
+          } else {
+            if (ready) {
+              return letterlist.slice(i+1,letterlist.length).join('') + letterlist.slice(0,i+1).join('')
+            }
+          }
+          curr = newpantig    
+        }
+      } 
+      // else return the original word
+      else {
+        return word
+      }
+
+    }
+}
+
+function lodify(sentence) {
+  // pad punctuation marks with space
+  // then split by space
+  const wordlist = sentence.replace(/['!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~']/g, ' $& ').split(' ') 
+  const lodified = wordlist.map(function (curr, ind, array) {
+    return lodifyWord(curr)
+  })
+  return lodified.join(' ').replace(/\s+(\W)/g, "$1")
+}
+
+
+
 
 
 http.listen(3000, function(){
